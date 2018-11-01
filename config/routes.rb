55ab166534fccce566df37ff12634fdffc61a7e2
Wouter Verhaeghe <wouter_verhaeghe@hotmail.com>
2018-11-01 17:28:54 +0100

    push end of day

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
+  resources :cocktails, only: [ :new, :create, :show ] do
+    resources :doses, only: [ :index, :new, :create, :destroy ]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
